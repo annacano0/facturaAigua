46a7cb7143d57b5ea9229ec4b0582b5413dc4e02
annacano0 <anna.elisabet5@gmail.com>
2024-01-10 09:16:44 +0100

    add: consoleColors.kt, Messages.kt, Utilities.kt

---
+++ src/main/kotlin/Messages.kt
@@ -0,0 +1,38 @@
+fun startOfProgramMessage(){
+    println(BLUE_BRIGHT+"----------------------------")
+    println("        Benvolgut/da        ")
+    println("----------------------------"+RESET)
+}
+
+/**
+ *This function prints goodbye message at the end of every iteration of program with blue bold color
+ * @author Thalia2603, annacano0
+ * @since 04/01/2024
+ */
+fun endOfProgramMessage(){
+    println(BLUE_BOLD_BRIGHT+"         Bon Viatge!        "+RESET)
+}
+
+/**
+ *This function prints Data Type error message (used in functions that check input) in red
+ * @author Thalia2603, annacano0
+ * @since 04/01/2024
+ */
+fun messageErrorDT(){
+    //funcion que es llamada cuando el tipo de dato es incorrecto
+    println(RED_BOLD+"El tipus de dada és incorrecte, torni a intentar."+RESET)
+}
+/**
+ *This function prints range error message (used in functions that check input) in yellow
+ * @author Thalia2603, annacano0
+ * @since 04/01/2024
+ */
+fun messageErrorRange(){
+    //funcion que es llamada cuando el tipo de dato esta fuera del rango establecido
+    println(YELLOW_BOLD+"El tipus de dada no està en les opcions, torni a intentar."+RESET)
+}
+
+fun messageExitProgram(){
+    println(RED_BOLD+"Has introduit el valor 4321"+RESET)
+    println(RED_BOLD+"FINALITZANT PROGRAMA"+RESET)
+}
---
+++ src/main/kotlin/Utilities.kt
@@ -0,0 +1,372 @@
+import java.util.*
+import kotlin.math.round
+val scan: Scanner = Scanner(System.`in`)
+
+/**
+ * This method can be used to read a String word value from the user through keyboard using java.util.Scanner
+ * @author raimon.izard
+ * @since 15/12/2023
+ * @param pMessageIn Input message to be shown to the user
+ * @param pMessageErrorDT Data type error message to be shown to the user
+ * @return outputValue Output value
+ */
+fun readWord(pMessageIn: String
+             , pMessageErrorDT: String
+): String{
+
+    var outputValue: String = ""
+    var correctDataType: Boolean = false
+
+    do{
+        println(pMessageIn)
+        correctDataType = scan.hasNext()
+
+        if (!correctDataType){
+            messageErrorDT()
+        }else{
+            outputValue = scan.next().lowercase()
+        }
+        scan.nextLine()
+    }while(!correctDataType)
+
+    return outputValue
+}
+
+/**
+ * This method can be used to read a String sentence from the user through keyboard using java.util.Scanner
+ * @author raimon.izard
+ * @since 15/12/2023
+ * @param pMessageIn Input message to be shown to the user
+ * @param pMessageErrorDT Data type error message to be shown to the user
+ * @return outputValue Output value
+ */
+fun readSentence(pMessageIn: String
+                 , pMessageErrorDT: String
+): String{
+
+    var outputValue: String = ""
+    var correctDataType: Boolean = false
+
+    do{
+        println(pMessageIn)
+        correctDataType = scan.hasNext()
+
+        if (!correctDataType){
+            messageErrorDT()
+        }else{
+            outputValue = scan.nextLine()
+        }
+    }while(!correctDataType)
+
+    return outputValue
+}
+
+/**
+ * This method can be used to read a Char value from the user through keyboard using java.util.Scanner
+ * @author raimon.izard
+ * @since 15/12/2023
+ * @param pMessageIn Input message to be shown to the user
+ * @param pMessageErrorDT Data type error message to be shown to the user
+ * @return outputValue Output value
+ */
+fun readChar(pMessageIn: String
+             , pMessageErrorDT: String
+): Char{
+
+    var outputValue: Char = ' '
+    var correctDataType: Boolean = false
+
+    do{
+        println(pMessageIn)
+        correctDataType = scan.hasNext()
+
+        if (!correctDataType){
+            messageErrorDT()
+        }else{
+            // Get the first character of the input word from scan.next()
+            outputValue = scan.next()[0]
+        }
+        scan.nextLine()
+    }while(!correctDataType)
+
+    return outputValue
+}
+
+/**
+ * This method can be used to read a Char value from the user through keyboard using java.util.Scanner. It can force the output Char to be lower or uppercase
+ * @author raimon.izard
+ * @since 15/12/2023
+ * @param pMessageIn Input message to be shown to the user
+ * @param pMessageErrorDT Data type error message to be shown to the user
+ * @param pCase Input parameter to force lowercase (0) or uppercase (1)
+ * @return outputValue Output value
+ */
+fun readChar(pMessageIn: String
+             , pMessageErrorDT: String
+             , pCase: Int
+): Char{
+
+    var outputValue: Char = ' '
+    var correctDataType: Boolean = false
+
+    do{
+        println(pMessageIn)
+        correctDataType = scan.hasNext()
+
+        if (!correctDataType){
+            messageErrorDT()
+        }else{
+            outputValue = if (pCase == 0) scan.next()[0].lowercaseChar()
+            else scan.next()[0].uppercaseChar()
+        }
+        scan.nextLine()
+    }while(!correctDataType)
+
+    return outputValue
+}
+
+/**
+ * This method can be used to read a Boolean value from the user through keyboard using java.util.Scanner
+ * @author raimon.izard
+ * @since 15/12/2023
+ * @param pMessageIn Input message to be shown to the user
+ * @return outputValue Output value
+ */
+fun readBoolean(pMessageIn: String
+): Boolean{
+
+    var outputValue: Boolean = false
+    var correctDataType: Boolean = false
+
+    do{
+        println(pMessageIn)
+        correctDataType = scan.hasNextBoolean()
+
+        if (!correctDataType){
+            messageErrorDT()
+        }else{
+            outputValue = scan.nextBoolean()
+        }
+        scan.nextLine()
+    }while(!correctDataType)
+
+    return outputValue
+}
+
+/**
+ * This method can be used to read an Int value from the user through keyboard using java.util.Scanner
+ * @author raimon.izard
+ * @since 15/12/2023
+ * @param pMessageIn Input message to be shown to the user
+ * @return outputValue Output value
+ */
+fun readInt(pMessageIn: String
+): Int{
+
+    var outputValue: Int = 0
+    var correctDataType: Boolean = false
+
+    do{
+        println(pMessageIn)
+        correctDataType = scan.hasNextInt()
+
+        if (!correctDataType){
+            messageErrorDT()
+        }else{
+            outputValue = scan.nextInt()
+        }
+        scan.nextLine()
+    }while(!correctDataType)
+
+    return outputValue
+}
+
+/**
+ * This method can be used to read an Int value from the user through keyboard using java.util.Scanner in within a range
+ * @author raimon.izard
+ * @since 15/12/2023
+ * @param pMessageIn Input message to be shown to the user
+ * @param pMin Min accepted value
+ * @param pMax Max accepted value
+ * @return outputValue Output value
+ */
+fun readIntRange(pMessageIn: String
+                 , pMin: Int
+                 , pMax: Int
+): Int{
+
+    var outputValue: Int = 0
+    var correctDataType: Boolean = false
+
+    do{
+        println(pMessageIn)
+        correctDataType = scan.hasNextInt()
+
+        if (!correctDataType){
+            messageErrorDT()
+        }else{
+            outputValue = scan.nextInt()
+
+            if (outputValue < pMin || outputValue > pMax){
+                messageErrorRange()
+                correctDataType = false
+            }
+        }
+        scan.nextLine()
+    }while(!correctDataType)
+
+    return outputValue
+}
+
+/**
+ * This method can be used to read a Float value from the user through keyboard using java.util.Scanner
+ * @author raimon.izard
+ * @since 15/12/2023
+ * @param pMessageIn Input message to be shown to the user
+ * @return outputValue Output value
+ */
+fun readFloat(pMessageIn: String
+): Float{
+
+    var outputValue: Float = 0.0f
+    var correctDataType: Boolean = false
+
+    do{
+        println(pMessageIn)
+        correctDataType = scan.hasNextFloat()
+
+        if (!correctDataType){
+            messageErrorDT()
+        }else{
+            outputValue = scan.nextFloat()
+        }
+        scan.nextLine()
+    }while(!correctDataType)
+
+    return outputValue
+}
+
+/**
+ * This method can be used to read a Float value from the user through keyboard using java.util.Scanner in within a range
+ * @author raimon.izard
+ * @since 15/12/2023
+ * @param pMessageIn Input message to be shown to the user
+ * @param pMin Min accepted value
+ * @param pMax Max accepted value
+ * @return outputValue Output value
+ */
+fun readFloat(pMessageIn: String
+              , pMin: Float
+              , pMax: Float
+): Float{
+
+    var outputValue: Float = 0.0f
+    var correctDataType: Boolean = false
+
+    do{
+        println(pMessageIn)
+        correctDataType = scan.hasNextFloat()
+
+        if (!correctDataType){
+            messageErrorDT()
+        }else{
+            outputValue = scan.nextFloat()
+
+            if (outputValue < pMin || outputValue > pMax){
+                messageErrorRange()
+                correctDataType = false
+            }
+        }
+        scan.nextLine()
+    }while(!correctDataType)
+
+    return outputValue
+}
+
+/**
+ * This method can be used to read a Float value from the user through keyboard using java.util.Scanner
+ * @author Thalia2603 anna.cano0
+ * @since 04/01/2024
+ * @return outputValue Output value
+ */
+/**
+ * This method can be used to read a Float value from the user through keyboard using java.util.Scanner
+ * If the user enters 4321, the program execution will terminate.
+ * @author Thalia2603, annacano0
+ * @since 04/01/2024
+ * @return outputValue Output value
+ */
+fun readFloat(): Float {
+    var outputValue: Float = 0.0f
+    var correctDataType: Boolean = false
+
+    do {
+        correctDataType = scan.hasNextFloat()
+
+        if (!correctDataType) {
+            messageErrorDT()
+        } else {
+            outputValue = scan.nextFloat()
+
+            // Check if the user entered 4321, and terminate the program if true
+            if (outputValue == 4321.0f) {
+                messageExitProgram()
+                System.exit(0)
+            }
+        }
+        scan.nextLine()
+    } while (!correctDataType)
+
+    return outputValue
+}
+
+/**
+ * This method can be used to round a Double "x" decimals and returns Float
+ * @author anna.cano0
+ * @since 23/12/2023
+ * @return inputUsuari Output value
+ */
+fun Double.round(decimals:Int): Float {
+    var multiplier = 1.0
+    repeat(decimals) { multiplier *= 10 }
+    return (round(this * multiplier) / multiplier).toFloat()
+}
+
+/**
+ * This method can be used to read a Si/No string value from the user through keyboard using java.util.Scanner
+ * and returns boolean
+ * @author annacano0
+ * @since 22/12/2023
+ * @param pMessageIn Input message to be shown to the user
+ * @param pMin Min accepted value
+ * @param pMax Max accepted value
+ * @return outputValue Output value (boolean)
+ */
+fun readStringSiNo(pMessageIn: String): Boolean {
+    var inputCorrecte: Boolean = true
+    var inputUsuari: String = ""
+    var outputValue: Boolean = false
+    do {
+        println(pMessageIn)
+        if (scan.hasNextLine()) {
+            inputCorrecte = true
+            inputUsuari = scan.nextLine().lowercase()
+            if (inputUsuari == "si") {
+                outputValue = true
+            } else if (inputUsuari == "no") {
+                outputValue = false
+            } else if (inputUsuari == "4321") {
+                messageExitProgram()
+                System.exit(0) // Finaliza la ejecución del programa
+            } else {
+                messageErrorDT()
+                inputCorrecte = false
+            }
+        } else {
+            scan.nextLine()
+            messageErrorRange()
+            inputCorrecte = false
+        }
+    } while (!inputCorrecte)
+    return outputValue
+}
